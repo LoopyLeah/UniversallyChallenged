2930808a91617a7b1c531eed254e5527afaff58f
malisyed29 <102291270+malisyed29@users.noreply.github.com>
2022-03-24 11:08:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-
+ res.send('Hello World!');
   for (let i = 1; i <= 10; i++) {
 if (i % 2 == 0) {
 res.send(i + " is even");
