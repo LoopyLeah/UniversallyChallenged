97de4b5ea946e2fff4ac2f799c216755ff83ac32
malisyed29 <102291270+malisyed29@users.noreply.github.com>
2022-03-24 11:05:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
-
-for (let i = 1; i <= 10; i++) {
+app.get('/', (req, res) => {
+ res.send('Hello World!');
+  for (let i = 1; i <= 10; i++) {
 if (i % 2 == 0) {
-console.log(i + " is even");
+res.send(i + " is even");
 } else {
-console.log(i + " is odd");
+res.send(i + " is odd");
 }
 }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
