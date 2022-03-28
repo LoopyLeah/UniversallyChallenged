ae80e8612d21836500c2fef8730d856b5440983e
LoopyLeah <102230369+LoopyLeah@users.noreply.github.com>
2022-03-28 17:21:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
- res.send('Hello World!');
-  for (let i = 1; i <= 10; i++) {
-if (i % 2 == 0) {
-res.send(i + " is even");
-} else {
-res.send(i + " is odd");
-}
-}
-});
+res.send('Hello World!');
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
