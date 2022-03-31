2cc81374226a8a42003ae6b900a59e9e2d13f009
LoopyLeah <102230369+LoopyLeah@users.noreply.github.com>
2022-03-31 10:03:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,22 @@
 const express = require('express')
+// create an express app
+const express = require("express")
 const app = express()
-const server = require('http').Server(app)
-const io = socketIO(server);
-var port = process.env.PORT || 8000;
-
-app.set('views', './views')
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-
-const rooms = { }
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.render('index', { rooms: rooms })
+  res.send('Hello World!')
+})
+// use the express-static middleware
+app.use(express.static("public"))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
 })
 
-app.post('/room', (req, res) => {
-  if (rooms[req.body.room] != null) {
-    return res.redirect('/')
-  }
-  rooms[req.body.room] = { users: {} }
-  res.redirect(req.body.room)
-  // Send message that new room was created
-  io.emit('room-created', req.body.room)
-})
-
-app.get('/:room', (req, res) => {
-  if (rooms[req.params.room] == null) {
-    return res.redirect('/')
-  }
-  res.render('room', { roomName: req.params.room })
-})
-
-server = http.Server(app);
-server.listen(port, () => {
-    console.log("App is running on port " + port);
-});
-
-io.on('connection', socket => {
-  socket.on('new-user', (room, name) => {
-    socket.join(room)
-    rooms[room].users[socket.id] = name
-    socket.to(room).broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', (room, message) => {
-    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    getUserRooms(socket).forEach(room => {
-      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
-      delete rooms[room].users[socket.id]
-    })
-  })
-})
-
-function getUserRooms(socket) {
-  return Object.entries(rooms).reduce((names, [name, room]) => {
-    if (room.users[socket.id] != null) names.push(name)
-    return names
-  }, [])
-}
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
