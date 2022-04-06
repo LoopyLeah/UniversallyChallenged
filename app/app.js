1234babda0cc75bfb569b4bca9a126b22122f19e
LoopyLeah <LeahHeffernan@protonmail.com>
2022-04-06 10:21:58 +0100

    Stealing richard's hard work

---
+++ app/app.js
@@ -0,0 +1,87 @@
+// const socket = io("http://localhost:3000/");
+const socket = io("https://oddoneoutgame.herokuapp.com/");
+
+console.log("server started");
+
+let newGamebutton = document.getElementById("new-game-button");
+let joinGamebutton = document.getElementById("join-game-button");
+let chooseGameDiv = document.getElementById("choose-game");
+let lobbyDiv = document.getElementById("lobby-content");
+let roomCode = document.getElementById("room-code");
+let message = document.getElementById("msg");
+let roomCodeHeader = document.getElementById("room-code-header");
+let startGameButton = document.getElementById("start-game-button");
+let gamePage = document.getElementById("game-page");
+let playerList = document.getElementById("players");
+let name = document.getElementById("name");
+// let categoryButton = document.getElementById("category-button");
+
+let hideContent = "none";
+let showContent = "block";
+
+//start new game on click
+newGamebutton.addEventListener("click", () => { 
+    console.log("new game button clicked");
+    handleGameButtonClick(hideContent, showContent, hideContent); //hide choosegame css, show lobby css, hide game css
+    let playerName = name.value;
+    socket.emit("new-game", playerName);
+});
+
+joinGamebutton.addEventListener("click", () => { 
+    console.log("join game button clicked");
+    //if the code is greater than 0 and a number
+    let roomCodeValue = roomCode.value;
+    if(roomCodeValue.length > 0 && !isNaN(roomCodeValue)){
+        handleGameButtonClick(hideContent, showContent, hideContent);
+        let playerName = name.value;
+        socket.emit("join-game", {code : roomCodeValue, name : playerName});
+    } else {
+        alert("Please enter a valid room code");
+    }
+});
+
+startGameButton.addEventListener("click", () => {
+    console.log("start game button clicked");
+    handleGameButtonClick(hideContent, hideContent, showContent);
+    socket.emit("start-game");
+});
+
+// btn.onclick = () => {
+//     let val = message.value; // value of the input
+//     console.log(val); // print value of the input
+//     // alert(`Your message: ${val}`);
+//     socket.emit('chat message', {
+//         message : val,
+//     });
+// };
+
+socket.on('message', data => {
+    let p = document.createElement("p");
+    p.innerHTML = data.message;
+    document.body.appendChild(p);
+});
+
+socket.on('room-code', data => {
+    console.log("room code: " + data);
+    roomCodeHeader.innerHTML = data;
+});
+
+//get the names of people in the lobby 
+socket.on('player-names', data => {
+    console.log("lobby names: " + data);
+    playerList.innerHTML = "";
+    //for each person in the lobby, create a p element in the list
+    for(let i = 0; i < data.length; i++) {
+        console.log(data[i].name);
+        let p = document.createElement("p");
+        p.innerHTML = `${i}. ${data[i].name}`;//number of person + name
+        playerList.appendChild(p); //add number and name to the list of players
+    }
+});
+
+function handleGameButtonClick(chooseGameStyle, lobbyStyle, gameStyle) {
+    console.log("game button clicked");
+    chooseGameDiv.style.display = chooseGameStyle;
+    lobbyDiv.style.display = lobbyStyle;
+    gamePage.style.display = gameStyle;
+}
---
+++ server.js
@@ -0,0 +1,99 @@
+let express = require('express'),
+  app = express(),
+  http = require('http'),
+  socketIO = require('socket.io'),
+  server, io;
+
+app.use(express.static(__dirname + '/app'));
+
+app.get('/', function(req, res) {
+  res.sendFile("app");
+});
+
+server = http.Server(app);
+server.listen(process.env.PORT || 3000);
+
+io = socketIO(server);
+
+//list of rooms, with the players
+let rooms = {};
+
+//when a person loads the website
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function (msg) {
+    console.log('message from ' + socket.id + ': ' + msg.message);
+    io.emit('message', msg);
+  });
+
+
+  //when a player enters a NEW room/game
+  socket.on('new-game', function(name) {
+    console.log("user id: " + socket.id);
+    //make a random 6 digit room code
+    let roomCode = Math.floor(Math.random() * 1000000);
+    console.log("room code: " + roomCode);
+    socket.join(roomCode);
+
+    //create a new room object
+    rooms[roomCode] = {
+      roomCode: roomCode,
+      players: [],
+      gameStarted: false,
+      gameOver: false,
+    }
+
+    //add the player object to the room
+    rooms[roomCode].players.push({
+      id: socket.id,
+      name: name,
+      score: 0,
+      ready: false,
+    });
+
+    console.log("player added to room: " + roomCode);
+    console.log(rooms);
+    console.log(rooms[roomCode].players);
+
+    socket.emit('room-code', roomCode);
+    sendPlayerNamesForLobby(roomCode);
+  });
+
+  //when a player JOINS an EXISTING room/game
+  socket.on('join-game', function(data) {
+    //console log the room code
+    console.log(data.code);
+    console.log(data.name);
+    //join the room
+    socket.join(data.code);
+
+    //add the player to the room
+    rooms[data.code].players.push({
+      id: socket.id,
+      name: data.name,
+      score: 0,
+      ready: false,
+    });
+
+    console.log("player added to room: " + data.code);
+    console.log(rooms[data.code].players);
+
+    socket.emit('room-code', data.code);
+    sendPlayerNamesForLobby(data.code);
+  })
+
+  //when the game is started
+  socket.on('start-game', function(){
+    console.log("starting game");
+    io.emit('game-started');
+  })
+
+});
+
+function sendPlayerNamesForLobby(roomCode) {
+  //send the player names to the lobby
+  io.sockets.in(roomCode).emit('player-names', rooms[roomCode].players);
+}
