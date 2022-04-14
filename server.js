a00511c1b866265f5ce82da8a01d4d5778a44215
LoopyLeah <102230369+LoopyLeah@users.noreply.github.com>
2022-04-14 19:23:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-const httpServer = require("http").createServer();
-const io = require("socket.io")(httpServer, {
-  // ...
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = 'app/app.js';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+
+
+
 
 
 app.use(express.static(__dirname + '/app'));
