22eb2ecab98fb318d2d5e1d4fc6ab84aefd84fd9
LoopyLeah <LeahHeffernan@protonmail.com>
2022-04-18 19:42:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 //Server-side file handling
 app.use(express.static('public'))
-app.listen(port)
+server.listen(port)
 console.log("Server running at:"+port);
 
 //Game server-side code
